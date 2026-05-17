2b328b6c4339b84e83a9b694e6e261fe092d7f2e
ostalkuba521-ops <ostalkuba521@gmail.com>
2026-05-17 16:26:42 +0200

    Update ExampleMixin.java

--- src/main/java/com/example/mixin/ExampleMixin.java
+++ src/main/java/com/example/mixin/ExampleMixin.java
@@ -1,15 +1,18 @@
-package com.example.mixin;
+package net.fabricmc.example.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.block.entity.SignBlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(MinecraftServer.class)
+@Mixin(SignBlockEntity.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadLevel")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadLevel()V
-	}
+    @Inject(method = "openTextEditor", at = @At("HEAD"), cancellable = true)
+    private void onOpenTextEditor(PlayerEntity player, boolean front, CallbackInfoReturnable<Boolean> cir) {
+        if (player.isSneaking()) {
+            cir.setReturnValue(false);
+        }
+    }
 }
